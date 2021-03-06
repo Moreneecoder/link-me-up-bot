bb2c84329e58b026872abc66a5d387ae019a0723
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-06 19:29:18 +0100

    Add #start_message method to BotMessage class

--- bin/bot_init.rb
+++ bin/bot_init.rb
@@ -2,14 +2,14 @@
 require 'dotenv'
 Dotenv.load('./.env')
 require 'telegram/bot'
-require './lib/bot_message.rb'
+require './lib/bot_message'
 
 token = ENV['BOT_API_KEY']
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     bot_message = BotMessage.new(message)
-
+    
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, parse_mode: 'MarkdownV2', text: bot_message.start_message )
--- lib/bot_message.rb
+++ lib/bot_message.rb
@@ -5,4 +5,10 @@ class BotMessage
     @message = message
   end
 
+  def start_message
+    "Hello *#{message.from.first_name}*\\. What will you like me to help you with?\n
+    If you don't already know the commands to engage with me, type /help and hit enter 
+    to see a list of tasks I can help you with\\."
+  end
+
 end
