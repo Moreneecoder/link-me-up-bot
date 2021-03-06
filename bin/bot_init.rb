41c4ab9e7c01d8dc06a5f00c13e358693fcdf877
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-06 18:12:01 +0100

    Add bot message class with initialize

--- bin/bot_init.rb
+++ bin/bot_init.rb
@@ -2,18 +2,21 @@
 require 'dotenv'
 Dotenv.load('./.env')
 require 'telegram/bot'
+require './lib/bot_message.rb'
 
 token = ENV['BOT_API_KEY']
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
+    bot_message = BotMessage.new(message)
+
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, parse_mode: 'MarkdownV2', text: "Hi, *#{message.from.first_name}*, What will you like me to help you with?")
+      bot.api.send_message(chat_id: message.chat.id, parse_mode: 'MarkdownV2', text: bot_message.start_message )
     when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, parse_mode: 'MarkdownV2', text: bot_message.stop_message )
     when '/help'
-        bot.api.send_message(chat_id: message.chat.id, text: "Which useless help you dey find")
+        bot.api.send_message(chat_id: message.chat.id, text: bot_message.help_message )
     end
   end
 end
---
+++ lib/bot_message.rb
@@ -0,0 +1,8 @@
+class BotMessage
+  attr_reader :message
+  
+  def initialize(message)
+    @message = message
+  end
+
+end
