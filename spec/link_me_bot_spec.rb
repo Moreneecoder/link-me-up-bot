0d5e542ef26aa272471ba0935a7cef4917a4b21b
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-07 02:49:40 +0100

    Fix rubocop errors

--- spec/link_me_bot_spec.rb
+++ spec/link_me_bot_spec.rb
@@ -13,14 +13,14 @@ describe LinkMeUp do
     end
 
     it 'returns false if argument is not /connect' do
-        link_up = LinkMeUp.new
-        expect(link_up.ready?('/help')).to be false
+      link_up = LinkMeUp.new
+      expect(link_up.ready?('/help')).to be false
     end
 
     it 'returns true if last command is /connect and current argument is a non-command string' do
-        link_up = LinkMeUp.new
-        link_up.ready?('/connect')
-        expect(link_up.ready?('football tech')).to be true
+      link_up = LinkMeUp.new
+      link_up.ready?('/connect')
+      expect(link_up.ready?('football tech')).to be true
     end
   end
 end
