b49164fec6bab3cbfaa25ba79492a5793e758372
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-11 10:11:34 +0100

    Add logic for #listen method in BotClient

--- lib/bot_client.rb
+++ lib/bot_client.rb
@@ -18,6 +18,26 @@ class BotClient
     end
   end
 
-  
+  private
+
+  def listen(bot)
+    bot.listen do |message|
+      bot_message = BotMessage.new(message)
+      respond_to_command(bot, message, bot_message)
+
+      connection_ready = @connect_request.ready?(message.text) && message.text != '/connect'
+
+      if connection_ready
+        if interests_not_above_5?(bot, message, bot_message)
+          puts "Interest not above five"
+        end
+      else
+        unless @connect_request.valid_command?(message)
+            bot.api.send_message(chat_id: message.chat.id, parse_mode: 'MarkdownV2', text: bot_message.help_message)
+          end
+      end
+      
+    end
+  end
 
 end
