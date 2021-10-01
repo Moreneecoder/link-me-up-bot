9234b3ac86b8c492dd1b4c54cb70c750eb6c3f64
Morenikeji Bello <caeserselassie@gmail.com>
2021-10-01 22:14:40 +0100

    Setup postgresql as db adapter

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 gem 'activerecord'
 gem 'dotenv', '~> 2.1', '>= 2.1.1'
+gem 'pg'
 gem 'rake'
 gem 'require_all'
 gem 'rubocop', '~>0.81.0'
 gem 'securerandom', '~> 0.1.0'
 gem 'sinatra'
 gem 'telegram-bot-ruby'
-gem 'pg'
 gem 'yaml'
 
 group :development do
--- persistence/db_connect.rb
+++ persistence/db_connect.rb
@@ -1,10 +1,10 @@
 require 'active_record'
 require 'pg'
 
-db_options = { 
-    adapter: 'postgresql',
-    host: 'localhost',
-    database: 'link_me_bot' ,
-    encoding: "unicode"
+db_options = {
+  adapter: 'postgresql',
+  host: 'localhost',
+  database: 'link_me_bot',
+  encoding: 'unicode'
 }
 ActiveRecord::Base.establish_connection(db_options)
