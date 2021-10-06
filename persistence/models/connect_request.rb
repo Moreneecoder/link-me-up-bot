7e88571ac94e228e55ed929bbcbafd4eee3226c3
Morenikeji Bello <caeserselassie@gmail.com>
2021-10-06 13:04:15 +0100

    Delete connect_request model

--- persistence/models/connect_request.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../db_connect'
-
-class ConnectRequest < ActiveRecord::Base
-  validates :chat_id, presence: true
-  validates :username, presence: true
-
-  # has_many :interests
-end
