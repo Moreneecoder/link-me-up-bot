6e70362917579afe7bfc9f28f51efd68c6f1274e
Morenikeji Bello <caeserselassie@gmail.com>
2021-10-06 13:24:17 +0100

    Tidy migrator task

--- persistence/models/interest.rb
+++ persistence/models/interest.rb
@@ -2,9 +2,8 @@ require_relative '../db_connect'
 
 class Interest < ActiveRecord::Base
   validates :title, presence: true
-  # validates :connect_request_id, presence: true
-
-  # belongs_to :connect_request
+  validates :chat_id, presence: true
+  validates :username, presence: true
 
   def match(limit = 1); end
 end
